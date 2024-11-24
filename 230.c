aa89a1665a835a27cee3383328ef96e081ef5190
Shiva prajapati <sp2314563@gmail.com>
2024-11-24 15:39:28 +0530

    Add files via upload

--- 230.c
+++ 230.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
 #include<conio.h>
 void main(){
-    int a[10],i,c;
+    int a[10],i,c,n;
+    c=0;
     printf("\n enter array");
     for(i=0;i<=9;i++){
         scanf("%d",&a[i]);
     }
-    for(i=0,i<=9;i++){
-        
+    printf("enter n=");
+    scanf("%d",&n);
+    for(i=0;i<=9;i++){
+         if(a[i]==n){
+            c++;
+         }
     }
+    printf("ferequancy of %d is %d",n,c);
+    getch();
 }
