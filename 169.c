8a98a6ed3477c856d8038089ea621deae665f283
Shiva prajapati <sp2314563@gmail.com>
2024-11-26 23:23:43 +0530

    Add files via upload

---
+++ 169.c
@@ -0,0 +1,38 @@
+//     **
+//    *  *
+//   *    *
+//  *      *
+// *        *
+// *        *
+//  *      *
+//   *    *
+//    *  *
+//     ** 
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int i,j,k;
+    for(i=5;i>=1;i--){
+        for(j=1;j<=i-1;j++){
+            printf(" ");
+        }
+        printf("*");
+        for(k=1;k<=10-2*i;k++){
+            printf(" ");
+        }
+        printf("*");
+        printf("\n");
+    }    
+    for(i=1;i<=5;i++){
+        for(j=1;j<i;j++){
+            printf(" ");
+        }
+        printf("*");
+        for(k=1;k<=10-2*i;k++){
+            printf(" ");
+        }
+        printf("*");
+        printf("\n");
+    }
+    getch();
+}   
